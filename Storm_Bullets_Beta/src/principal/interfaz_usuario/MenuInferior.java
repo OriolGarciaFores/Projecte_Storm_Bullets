3709783f6cd0fdba5dacebae4d8426b310962fbb
OriolGarciaFores <ori_ugf@hotmail.com>
2016-11-13 14:47:56 +0100

    Timer

--- Storm_Bullets_Beta/src/principal/interfaz_usuario/MenuInferior.java
+++ Storm_Bullets_Beta/src/principal/interfaz_usuario/MenuInferior.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import principal.Constantes;
 import principal.entes.Jugador;
 import principal.herramientas.CargadorRecursos;
@@ -14,7 +16,11 @@ import principal.herramientas.DibujoDebug;
 public class MenuInferior {
     private Rectangle areaInventario;
     private Rectangle bordeAreaInventario;
-    
+    //Pruebas.
+    private Integer minutos = 0 , segundos = 0;
+    private String min="00", seg="00";
+    private int NS_POR_SEGUNDO;
+    //Fin de pruebas.
     private Color negroDesaturado;
     private Color rojoOscuro;
     private static final BufferedImage img = CargadorRecursos.cargarImagenCompatibleTranslucida(Constantes.RUTA_AVATAR);
@@ -28,7 +34,7 @@ public class MenuInferior {
         rojoOscuro = new Color(150, 0, 0);
     }
     
-    public void dibujar(final Graphics g, final Jugador jugador){
+    public void dibujar(final Graphics g, final Jugador jugador) {
         dibujarAreaInventario(g);
         dibujarBarraVitalidad(g, jugador);
         dibujarRanurasObjetos(g);
@@ -40,7 +46,7 @@ public class MenuInferior {
         DibujoDebug.dibujarRectanguloRelleno(g, bordeAreaInventario, Color.white);
     }
     
-    private void dibujarBarraVitalidad(final Graphics g, Jugador jugador){
+    private void dibujarBarraVitalidad(final Graphics g, Jugador jugador) {
         final int medidaVertical = 10;
         final int anchoTotal = 200;
        
@@ -50,7 +56,11 @@ public class MenuInferior {
         DibujoDebug.dibujarString(g, "HP", areaInventario.x + 125, areaInventario.y + medidaVertical * 2);
         DibujoDebug.dibujarString(g, "Jugador", areaInventario.x + 5 , areaInventario.y + 54);
         DibujoDebug.dibujarString(g, jugador.obtenerVidaJugador(), areaInventario.x + 150, areaInventario.y + medidaVertical * 2);
-        DibujoDebug.dibujarString(g, "Tiempo: 0 ", areaInventario.x + 50, areaInventario.y + medidaVertical * 4);
+        try {
+            DibujoDebug.dibujarString(g, "Tiempo: " + tiempo(), areaInventario.x + 50, areaInventario.y + medidaVertical * 4);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MenuInferior.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     private void dibujarRanurasObjetos(final Graphics g){
@@ -80,6 +90,47 @@ public class MenuInferior {
         DibujoDebug.dibujarImagen(g, DibujoDebug.imagenRedimensionada(img, anchoRanura, anchoRanura), espacioRanura, areaInventario.y + 4);
     }
     
-     
+    private String tiempo() throws InterruptedException{
+                
+                      
+                //Incrementamos 4 milesimas de segundo
+                
+                 NS_POR_SEGUNDO++;
+                 
+                //Cuando llega a 1000 osea 1 segundo aumenta 1 segundo
+                //y las milesimas de segundo de nuevo a 0
+                if( NS_POR_SEGUNDO == 700)
+                {
+                    NS_POR_SEGUNDO = 0;
+                    segundos += 1;
+                    if(segundos < 10){
+                        seg = "0" + String.valueOf(segundos);
+                    } else {
+                        seg = String.valueOf(segundos);
+                    }
+                    
+                   // System.out.println(seg);
+                    //Si los segundos llegan a 60 entonces aumenta 1 los minutos
+                    //y los segundos vuelven a 0
+                    if( segundos == 60 )
+                    {
+                        segundos = 0;
+                        minutos++;
+                        
+                        if(minutos < 10){
+                        min = "0" + Integer.toString(minutos);
+                        }else{
+                            min = Integer.toString(minutos);
+                        }
+                    }
+                }
+                 
+                
+        return min + ":" + seg ;
+    
+            
+    }
+    
+
     
 }
